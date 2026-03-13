c7883a96f76b2273d76f6403758adcafc25b2e94
Kavana H P <kavanagangadhar267@gmail.com>
2026-03-13 09:50:48 +0530

    Time: 3 ms (54.81%), Space: 9.3 MB (12.12%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,14 @@
+int reverse(int x){
+    int r,rev=0;
+    while(x!=0)
+    {
+        r=x%10;
+        x=x/10;
+        if(rev>INT_MAX/10|| rev<INT_MIN/10)
+        {
+            return 0;
+        }
+        rev=rev*10+r;
+    }
+    return rev;
+}
