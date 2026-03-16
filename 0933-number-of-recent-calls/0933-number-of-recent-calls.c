f71049aace815a34f32380d9bfacc809153d76cf
Kavana H P <kavanagangadhar267@gmail.com>
2026-03-16 22:40:26 +0530

    Time: 14 ms (76.63%), Space: 41.9 MB (50.89%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.c
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.c
@@ -1,33 +1,40 @@
-#define MAX_SIZE 10000
 
 typedef struct {
-    int* pingHistory;
-    int head;
-    int tail; 
+    int ulHeadPtr;
+    int ulTailPtr;
+    int recent_t[3002];
 } RecentCounter;
 
 RecentCounter* recentCounterCreate() {
-    RecentCounter* obj = malloc(sizeof(RecentCounter));
-    obj -> pingHistory = calloc(10000, sizeof(int));
-    obj -> head = 0;
-    obj -> tail = 0;
-    return obj;
+    RecentCounter* rcc_ptr = (RecentCounter*)malloc (sizeof(RecentCounter));
+    rcc_ptr->ulHeadPtr = 0;
+    rcc_ptr->ulTailPtr = 0;
+    return rcc_ptr;
 }
 
 int recentCounterPing(RecentCounter* obj, int t) {
-    obj -> pingHistory[obj->head++] = t;
-    
-    for(; obj->tail < obj -> head; obj->tail++)
-    {
-        if((t-3000) <= obj -> pingHistory[obj->tail])
-        {
-            break;
+    obj->recent_t[obj->ulHeadPtr] = t;
+    obj->ulHeadPtr++;
+    if(obj->ulHeadPtr == 3002){
+        obj->ulHeadPtr = 0;
+    }
+    while(obj->recent_t[obj->ulTailPtr] < (t-3000)){
+        obj->ulTailPtr++;
+        if(obj->ulTailPtr == 3002){
+            obj->ulTailPtr = 0;
         }
     }
-    return obj->head-obj->tail;
+    return ((obj->ulHeadPtr >= obj->ulTailPtr) ?  (obj->ulHeadPtr-obj->ulTailPtr) : (obj->ulHeadPtr-obj->ulTailPtr + 3002));
 }
 
 void recentCounterFree(RecentCounter* obj) {
-    free(obj->pingHistory);
-    free(obj);
+   free(obj);
 }
+
+/**
+ * Your RecentCounter struct will be instantiated and called as such:
+ * RecentCounter* obj = recentCounterCreate();
+ * int param_1 = recentCounterPing(obj, t);
+ 
+ * recentCounterFree(obj);
+*/
